be32aa83d174f655e1dbf2bd85e3d911b1b50bb3
Miguel E. Ruiz <meruiz77@gmail.com>
2021-12-08 23:28:07 +0100

    Ejemplo4 para N procesos

--- practica3/Ejemplo4/heat.c
+++ practica3/Ejemplo4/heat.c
@@ -43,20 +43,20 @@ static void init(unsigned int source_x, unsigned int source_y, float * matrix) {
 	}
 }
 
-// TODO añadir world_size para el caso de N procesos
-static void step(unsigned int source_x, unsigned int source_y, const float * current, float * next, int rank) {
+static void step(unsigned int source_x, unsigned int source_y, const float * current, float * next, int rank, int w_size) {
 
 	float recv_ghost_row[N];
 	float send_ghost_row[N];
-	unsigned int half = (N/2);
-	unsigned int last_row = half - 1;
+	float recv_ghost_row1[N];
+	float send_ghost_row1[N];
+	unsigned int slice = (N/w_size);
+	unsigned int last_row = (slice*rank)+(slice-1);
 
 	MPI_Status status;
 
-	// TODO split logic from rank == 0 and otherwise case
 	if (rank == 0)
 	{
-		for (unsigned int y = 1; y < half-1; ++y) {
+		for (unsigned int y = 1; y < slice-1; ++y) {
 			for (unsigned int x = 1; x < N-1; ++x) {
 
 				if ((y == source_y) && (x == source_x)) {
@@ -74,6 +74,9 @@ static void step(unsigned int source_x, unsigned int source_y, const float * cur
 		// Se procesa la última fila
 		for (unsigned int x = 1, y = last_row; x < N - 1; ++x)
 		{
+			if ((y == source_y) && (x == source_x)) {
+					continue;
+				}
 			next[idx(x, y, N)] = (current[idx(x, y-1, N)] +
 															current[idx(x-1, y, N)] +
 															current[idx(x+1, y, N)] +
@@ -87,28 +90,52 @@ static void step(unsigned int source_x, unsigned int source_y, const float * cur
 
 		MPI_Send( send_ghost_row , N , MPI_FLOAT , 1 , 0 , MPI_COMM_WORLD);
 
-	} else
-	{
-		// Envio de la fila ghost para el proceso maestro
+	} else if(0 < rank && rank < w_size-1) {
+		int former = rank - 1;
+		int latter = rank + 1;
+		// Enviar y recibir con su anterior
+
+		// Enviar y recibir con su posterior
+		MPI_Recv(recv_ghost_row1, N, MPI_FLOAT, latter, latter, MPI_COMM_WORLD, &status);
+
+		for (unsigned int x = 1, y = last_row; x < N - 1; ++x)
+		{
+			if ((y == source_y) && (x == source_x)) {
+					continue;
+			}
+			next[idx(x, y, N)] = (current[idx(x, y-1, N)] +
+															current[idx(x-1, y, N)] +
+															current[idx(x+1, y, N)] +
+															recv_ghost_row1[x]) / 4.0f;
+		}
+
+		for (unsigned int x = 0; x < N - 1; ++x)
+		{
+			send_ghost_row1[x] = current[idx(x, last_row, N)];
+		}
+
+		MPI_Send( send_ghost_row1 , N , MPI_FLOAT , latter , rank , MPI_COMM_WORLD);
 		for (unsigned int x = 0; x < N; ++x)
 		{
-			send_ghost_row[x] = current[idx(x, half, N)];
+			send_ghost_row[x] = current[idx(x, slice*rank, N)];
 		}
+		MPI_Send( send_ghost_row , N , MPI_FLOAT , former , rank , MPI_COMM_WORLD);
 
-		MPI_Send( send_ghost_row , N , MPI_FLOAT , 0 , 1 , MPI_COMM_WORLD);
+		// Recibo de la fila ghost del proceso anterior
+		MPI_Recv( recv_ghost_row , N , MPI_FLOAT , former , former , MPI_COMM_WORLD , &status);
 
-		// Recibo de la fila ghost del proceso maestro
-		MPI_Recv( recv_ghost_row , N , MPI_FLOAT , 0 , 0 , MPI_COMM_WORLD , &status);
+		for (unsigned int x = 1, y = slice * rank; x < N - 1; ++x)
+			{
+				if ((y == source_y) && (x == source_x)) {
+					continue;
+				}
+				next[idx(x, y, N)] = (recv_ghost_row[x] +
+															current[idx(x-1, y, N)] +
+															current[idx(x+1, y, N)] +
+															current[idx(x, y+1, N)]) / 4.0f;
+			}
 
-		for (unsigned int x = 1, y = half; x < N - 1; ++x)
-		{
-			next[idx(x, y, N)] = (recv_ghost_row[x] +
-														current[idx(x-1, y, N)] +
-														current[idx(x+1, y, N)] +
-														current[idx(x, y+1, N)]) / 4.0f;
-		}
-
-		for (unsigned int y = half + 1; y < N-1; ++y) {
+		for (unsigned int y = slice*rank+1; y < (rank*slice)+(slice-1); ++y) {
 			for (unsigned int x = 1; x < N-1; ++x) {
 
 				if ((y == source_y) && (x == source_x)) {
@@ -120,24 +147,57 @@ static void step(unsigned int source_x, unsigned int source_y, const float * cur
 															current[idx(x, y+1, N)]) / 4.0f;
 			}
 		}
+	} else {
+		// Envio de la fila ghost para el proceso maestro
+		for (unsigned int x = 0; x < N; ++x)
+		{
+			send_ghost_row[x] = current[idx(x, slice*rank, N)];
+		}
+		MPI_Send( send_ghost_row , N , MPI_FLOAT , rank-1 , rank , MPI_COMM_WORLD);
+		// Recibo de la fila ghost del proceso maestro
+		MPI_Recv( recv_ghost_row , N , MPI_FLOAT , rank-1 , rank-1 , MPI_COMM_WORLD , &status);
+		for (unsigned int x = 1, y = slice*rank; x < N - 1; ++x)
+		{
+			if ((y == source_y) && (x == source_x)) {
+				continue;
+			}
+			next[idx(x, y, N)] = (recv_ghost_row[x] +
+														current[idx(x-1, y, N)] +
+														current[idx(x+1, y, N)] +
+														current[idx(x, y+1, N)]) / 4.0f;
+		}
+		for (unsigned int y = slice*rank + 1; y < N-1; ++y) {
+			for (unsigned int x = 1; x < N-1; ++x) {
+				if ((y == source_y) && (x == source_x)) {
+					continue;
+				}
+				next[idx(x, y, N)] = (current[idx(x, y-1, N)] +
+															current[idx(x-1, y, N)] +
+															current[idx(x+1, y, N)] +
+															current[idx(x, y+1, N)]) / 4.0f;
+			}
+		}
 	}
-
 }
 
-// TODO añadir world_size para el caso de N procesos
-static float diff(const float * current, const float * next, int rank) {
-	int half = N/2 ;
+static float diff(const float * current, const float * next, int rank, int w_size) {
+	int slice = (N/w_size) ;
 	float maxdiff = 0.0f;
 	if (rank == 0)
 	{
-		for (unsigned int y = 1; y < half; ++y) {
+		for (unsigned int y = 1; y < slice; ++y) {
 			for (unsigned int x = 1; x < N-1; ++x) {
 				maxdiff = fmaxf(maxdiff, fabsf(next[idx(x, y, N)] - current[idx(x, y, N)]));
 			}
 		}
-	} else
-	{
-		for (unsigned int y = half; y < N-1; ++y) {
+	} else if(0 < rank && rank < w_size) {
+		for (unsigned int y = 1; y < slice * rank+slice; ++y) {
+			for (unsigned int x = 1; x < N-1; ++x) {
+				maxdiff = fmaxf(maxdiff, fabsf(next[idx(x, y, N)] - current[idx(x, y, N)]));
+			}
+		}
+	} else {
+		for (unsigned int y = slice; y < N-1; ++y) {
 			for (unsigned int x = 1; x < N-1; ++x) {
 				maxdiff = fmaxf(maxdiff, fabsf(next[idx(x, y, N)] - current[idx(x, y, N)]));
 			}
@@ -200,8 +260,8 @@ int main() {
 	float global_t_diff = SOURCE_TEMP;
 
 	for (unsigned int it = 0; (it < MAX_ITERATIONS) && (global_t_diff > MIN_DELTA); ++it) {
-		step(source_x, source_y, current, next, world_rank);
-		t_diff = diff(current, next, world_rank);
+		step(source_x, source_y, current, next, world_rank, world_size);
+		t_diff = diff(current, next, world_rank, world_size);
 		MPI_Allreduce(&t_diff, &global_t_diff, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD);
 		if(it%(MAX_ITERATIONS/10)==0 && world_rank == 0){
 			printf("%u: %f\n", it, global_t_diff);
@@ -214,17 +274,20 @@ int main() {
 
 	if(world_rank==0){
 
+		unsigned int slice = (N/world_size);
 		double stop = MPI_Wtime();
 
 		printf("Computing time %f s.\n", stop-start);
 
 		float * aux = malloc(array_size);
 
-		MPI_Recv( aux , N*N , MPI_FLOAT , 1 , 1 , MPI_COMM_WORLD , &status);
-
-		for (unsigned int y = (N/2); y < N; ++y) {
-			for (unsigned int x = 0; x < N; ++x) {
-				current[idx(x, y, N)] = aux[idx(x, y, N)];
+		for (unsigned int i = 1; i < world_size; i++)
+		{
+			MPI_Recv( aux , N*N , MPI_FLOAT , i , 1 , MPI_COMM_WORLD , &status);
+			for (unsigned int y = slice*i; y < (i == (world_size-1) ? N-1 : (slice*i)+slice); ++y) {
+				for (unsigned int x = 0; x < N; ++x) {
+					current[idx(x, y, N)] = aux[idx(x, y, N)];
+				}
 			}
 		}
 
